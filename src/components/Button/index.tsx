61b6713dc52d44f198a1f5c82b8aedfcc777953a
AFINITI\sallar.hussain <sallar.hussain@afiniti.com>
2022-04-28 02:37:00 +0500

    Add custom Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Button as MuiButton } from "@mui/material";
+
+const Button = styled(MuiButton)`
+  color: white !important;
+`;
+
+export default Button;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,6 @@ import AppBar from "./AppBar";
 import Drawer from "./Drawer";
 import Layout from "./Layout";
 import TextField from "./TextField";
+import Button from "./Button";
 
-export { AppBar, Drawer, Layout, TextField };
+export { AppBar, Drawer, Layout, TextField, Button };
