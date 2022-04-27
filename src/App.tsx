e8d76dcf0eb7cf55adadf71157fd323344b34b2c
AFINITI\sallar.hussain <sallar.hussain@afiniti.com>
2022-04-27 06:40:03 +0500

    Added Mui Theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,28 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/store";
 import Routes from "./routes";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { green, orange } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: green[400],
+    },
+    secondary: {
+      main: orange[500],
+    },
+  },
+});
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes></Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes></Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   );
 }
