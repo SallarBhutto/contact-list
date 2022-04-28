779db45208747d06545559be3a13daf7a97ba95d
AFINITI\sallar.hussain <sallar.hussain@afiniti.com>
2022-04-28 10:12:12 +0500

    Add providers in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
 import React from "react";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import { store } from "./app/store";
 import Routes from "./routes";
-import { createTheme, ThemeProvider } from "@mui/material";
-import { green } from "@mui/material/colors";
-
-export const theme = createTheme({
-  palette: {
-    primary: {
-      main: green[400],
-    },
-    secondary: {
-      main: green[100],
-    },
-  },
-});
+import Providers from "./providers";
 
 function App() {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Routes></Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </Provider>
+    <Providers>
+      <Routes />
+    </Providers>
   );
 }
 
