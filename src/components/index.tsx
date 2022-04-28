edb868b95d1634f87179930731696f8a52c83b1b
AFINITI\sallar.hussain <sallar.hussain@afiniti.com>
2022-04-28 10:16:36 +0500

    Update components index for new added components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,17 @@ import Drawer from "./Drawer";
 import Layout from "./Layout";
 import TextField from "./TextField";
 import Button from "./Button";
+import ContactList from "./ContactList";
+import ContactRow from "./ContactRow";
+import PageContentContainer from "./PageContentContainer";
 
-export { AppBar, Drawer, Layout, TextField, Button };
+export {
+  AppBar,
+  Drawer,
+  Layout,
+  TextField,
+  Button,
+  ContactList,
+  ContactRow,
+  PageContentContainer,
+};
