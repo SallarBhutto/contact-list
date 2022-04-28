876276535165f1a4baa1ebd72342c3f282091792
AFINITI\sallar.hussain <sallar.hussain@afiniti.com>
2022-04-28 10:15:21 +0500

    Add pageContainerComponent for page contents

---
+++ src/components/PageContentContainer/index.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { Grid } from "@mui/material";
+
+const PageContentContainer = styled(Grid)`
+  background-color: white;
+  display: flex;
+  flex: 1;
+  flexgrow: 1;
+  margin-top: 25px;
+  flex-direction: column;
+  padding: 25px;
+`;
+
+export default PageContentContainer;
