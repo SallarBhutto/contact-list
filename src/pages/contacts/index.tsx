154c5eab3e6a01d46fcdc7e4be88112303e346d9
AFINITI\sallar.hussain <sallar.hussain@afiniti.com>
2022-04-28 10:17:51 +0500

    Add contact page

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -1,13 +1,41 @@
-import React from "react";
-import Layout from "../../components/Layout";
-import { Box } from "@mui/material";
+import React, { FC } from "react";
+import {
+  Layout,
+  TextField,
+  Button,
+  ContactList,
+  PageContentContainer,
+} from "../../components";
+import { Grid } from "@mui/material";
+import { Add } from "@mui/icons-material";
 
-function Contacts() {
+const Contacts: FC = () => {
   return (
     <Layout>
-      <Box>Contacts page</Box>
+      <PageContentContainer>
+        {/* Search and Add Contact */}
+        <Grid alignItems={"center"} container>
+          <Grid item xs={10}>
+            <TextField
+              size="small"
+              fullWidth
+              name="Search"
+              variant="outlined"
+              placeholder="Search contacts"
+              autoComplete="on"
+            />
+          </Grid>
+          <Grid display={"flex"} justifyContent={"flex-end"} item xs={2}>
+            <Button variant="contained" startIcon={<Add />}>
+              Add Contact
+            </Button>
+          </Grid>
+        </Grid>
+
+        <ContactList />
+      </PageContentContainer>
     </Layout>
   );
-}
+};
 
 export default Contacts;
